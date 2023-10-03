a5646ce8c0662669260ae6a5eee1a9c85e48474d
Loriano Storchi <loriano@storchi.org>
2023-10-03 13:22:33 +0200

    to extract communication timne CPU to GPU

--- extract_time_pyberthart.sh
+++ extract_time_pyberthart.sh
@@ -27,5 +27,13 @@ export SUM=$(grep "Iteration" $1 | grep "\ of\ " | awk '{print $6}' | awk '{s+=$
 export MEAN=$(calc $SUM/$NUM)
 Iteration=$MEAN
 
-echo "Fock , Exp , MO_Fock , Mainloop , Iteration "
-echo $Fock " , " $Exp " , " $MOFOCK " , " $Mainloop " , " $Iteration
+export SUM=$(grep "Time_main_communication:" $1 | awk '{print $2}')
+Maincomm=$SUM
+
+export NUM=$(grep "Time_step_communication:" $1 | awk '{print $2}' | wc -l)
+export SUM=$(grep "Time_step_communication:" $1 | awk '{print $2}' | awk '{s+=$1} END {print s}')
+export MEAN=$(calc $SUM/$NUM)
+Commiter=$MEAN
+
+echo "Fock , Exp , MO_Fock , Mainloop , Maincommunication , Itercommunication ,  Iteration "
+echo $Fock " , " $Exp " , " $MOFOCK " , " $Mainloop " , " $Maincomm " , " $Commiter  " , "  $Iteration
